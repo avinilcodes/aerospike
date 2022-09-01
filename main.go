ede101d9c5c73d3048e49845e30f26f337bff5fc
avinilcodes <avinilbedarkar@gmail.com>
2022-09-01 19:14:22 +0530

    Initial commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	as "github.com/aerospike/aerospike-client-go"
+)
+
+func main() {
+	client, err := as.NewClient("127.0.0.1", 3000)
+	if err != nil {
+		panic(err)
+	}
+	defer client.Close()
+	policy := as.NewWritePolicy(0, 0)
+
+	key, _ := as.NewKey("test", "myset", "mykey")
+	key1, _ := as.NewKey("test", "myset", "mykey1")
+	key2, _ := as.NewKey("test", "myset", "mykey2")
+
+	bin := as.NewBin("mybin", "myvalue")
+
+	client.PutBins(policy, key2, bin)
+	bin1 := as.NewBin("name", "John")
+	bin2 := as.NewBin("age", 25)
+
+	client.PutBins(policy, key2, bin1, bin2)
+	bin3 := as.NewBin("electronics", "Phone")
+	client.PutBins(policy, key, bin3)
+
+	bin4 := as.NewBin("electronics", "Tv")
+	client.PutBins(policy, key1, bin4)
+
+	bin5 := as.NewBin("electronics", "WM")
+	client.PutBins(policy, key2, bin5)
+
+	bin6 := as.NewBin("date", "25/06/2022")
+	client.PutBins(policy, key2, bin6)
+	bin7 := as.NewBin("Salary", 1000.0)
+	client.PutBins(policy, key2, bin7)
+	bin8 := as.NewBin("Currency", "USD")
+	client.PutBins(policy, key2, bin8)
+}
